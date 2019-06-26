80069cf7c31217cb2fd942d0358c53357d3cb1bf
Mikhail Migushov <me@chuvash.pw>
2019-06-27 01:53:01 +0300

    Parse args, read data from serial, send data in HTTP POST to url

---
+++ receive-ardata.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+from datetime import datetime
+
+import serial
+import requests
+
+
+def main(*args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--baudrate', type=int, default=9600)
+    parser.add_argument('-p', '--port', required=True)
+    parser.add_argument('-u', '--data-url', required=True)
+    argv = parser.parse_args(sys.argv[1:] or args)
+
+    with serial.Serial(port=argv.port, baudrate=argv.baudrate, timeout=10) as ser:
+        with requests.Session() as s:
+            while True:
+                value = ser.readline().decode()
+                print(datetime.now().isoformat(timespec='seconds'), value)
+                res = s.post(argv.data_url,
+                             json={
+                                 'data': {
+                                     'value': value,
+                                     'date': datetime.now().isoformat(timespec='seconds')
+                                 }
+                             })
+                res.raise_for_status()
+
+
+if __name__ == '__main__':
+    main()
